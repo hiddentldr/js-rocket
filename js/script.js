849b6e81ba9f1d135a8fae9fae8da29d475b2bb7
Erdős Tamás <tomocska602@gmail.com>
2017-10-26 20:27:56 +0200

    Made some stuff
    Cars and button

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+      var car = {
+
+        make: 'VW',
+        type: 'Polo',
+        color: 'blue',
+        isTurnedOn: false,
+        numberOfWheels: 4,
+        seats: [
+          'seat1',
+          'seat2',
+          'seat3',
+          'seat4',
+        ],
+        turnOn: function () {
+          this.isTurnedOn = true;
+          alert('Car is now turned on');
+        },
+        fly: function () {
+          alert('FLY');
+        },
+        switchCar: function (isOn) {
+          console.log('turn car ' + isOn);
+          if (isOn == true) {
+            this.isTurnedOn = true;
+          } else {
+            this.isTurnedOn = false;
+          }
+        }
+      };
+
+      console.log('Hello there friends');
